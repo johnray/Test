435c7f81bddc01df461cb6e72b8c6eaa30c56638
John E. Ray <jray@Johns-iMacish.local>
2013-11-03 09:02:02 -0500

    A sample commit message

---
+++ Sample2/Sample2Tests/Sample2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Sample2Tests.m
+//  Sample2Tests
+//
+//  Created by John E. Ray on 11/3/13.
+//  Copyright (c) 2013 John E. Ray. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Sample2Tests : XCTestCase
+
+@end
+
+@implementation Sample2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
