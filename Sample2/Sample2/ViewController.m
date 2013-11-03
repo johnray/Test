c7119cd17203538127b8640582bf7da7ca0f63a5
John E. Ray <jray@Johns-iMacish.local>
2013-11-03 08:44:16 -0500

    Example commit

---
+++ Sample2/Sample2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Sample2
+//
+//  Created by John E. Ray on 11/3/13.
+//  Copyright (c) 2013 John E. Ray. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+// Just an example addition
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
